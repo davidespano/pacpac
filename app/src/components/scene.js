ed78f7287f9676f2dc3f89b32569317c69b65ff3
f-fanni <fanni.filippo@gmail.com>
2018-06-08 17:23:18 +0200

    minor

--- app/src/components/scene.js
+++ app/src/components/scene.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Scene(props){
     return(
         <div className={'scene'}>
-            <img src="./Image360/sample1.jpg"/>
+            <img src="./Image360/sample1.jpg" alt={'alena'}/>
         </div>
     );
 }
