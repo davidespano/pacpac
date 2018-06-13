f000d2de5c744b4f7ba96d4bf5538cdb1f31e81a
alessandroTola <alessandro.tola@yahoo.it>
2018-06-13 18:36:06 +0200

    Problem react geometry

--- app/src/components/aframe-selectable.js
+++ app/src/components/aframe-selectable.js
@@ -1,4 +1,6 @@
-var AFRAME = require('aframe')
+import { registerComponent } from 'aframe'
+
+var AFRAME = require('aframe');
 
 AFRAME.registerComponent('selectable', {
 
@@ -6,24 +8,21 @@ AFRAME.registerComponent('selectable', {
 
         var sceneEl = document.querySelector('a-scene');
         var curv = sceneEl.querySelector('#curved');
-        var feedBackActivator = sceneEl.querySelector('#feedback');
-        var skyChanger = sceneEl.querySelector('#blabla');
         var template = sceneEl.querySelector('#templateBello');
-        var templateName = this.data;
         var template2 = sceneEl.querySelector('#templateBello2');
 
 
-        curv.addEventListener('mouseenter', function (evt) {
+        /*curv.addEventListener('mouseenter', function (evt) {
             feedBackActivator.setAttribute('material', 'visible', true);
         });
         curv.addEventListener('mouseleave', function (evt) {
             feedBackActivator.setAttribute('material', 'visible', false);
-        });
+        });*/
 
         curv.addEventListener('click', function(evt){
             template2.setAttribute('visible', true);
             template.setAttribute('visible', false);
-            feedBackActivator.disable();
+            //feedBackActivator.disable();
         });
     }
 });
