5ad01a7631791e931d4cb2d18f7264a9de4d5716
martinasenis <martina.senis.92@gmail.com>
2018-06-15 16:32:35 +0200

    package

--- app/src/components/topbar.js
+++ app/src/components/topbar.js
@@ -15,8 +15,8 @@ function TopBar(props){
                                 <label htmlFor={"image"}>Scegli una immagine</label>
                                 <input type="file" name="image" id="image"/>
                                 <button onClick={()=>{
-                                 var input=getInput();
-                                 props.addScene(input.name,input.name,input.img);
+                                    var input=getInput();
+                                    props.addScene(input.name,input.name,input.img);
                                 }
                                 }>Conferma</button>
                         </div>
@@ -31,12 +31,36 @@ function getInput() {
 
     var scene_name=document.getElementById("scene_name").value;
     var img=document.getElementById("image").value;
-    console.log(scene_name,img)
+    console.log(scene_name,img);
+    getURL();
     return {
         name: scene_name,
         img: img,
     }
     //Aggiungere libreria per leggere l'URL dell'immagine.
 }
+
+function getURL(){
+    var img=document.getElementById('image');
+
+    if(img.files && img.files[0]){
+        var reader = new FileReader();
+    }
+
+    console.log(reader.readAsDataURL(img.files[0]));
+}
+
+/*function readURL(input) {
+
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('#blah').attr('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}*/
 
 export default TopBar;
