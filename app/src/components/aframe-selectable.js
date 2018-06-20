69f148691a5e336c8ba5f43be9df5bfc29c4167f
Vaknar <Vaknarosh@gmail.com>
2018-06-20 12:27:49 +0200

    Changed from a-animation to aframe-animation-component!

--- app/src/components/aframe-selectable.js
+++ app/src/components/aframe-selectable.js
@@ -1,6 +1,9 @@
+import 'aframe-animation-component';
+
 var AFRAME = require('aframe');
 var utils = AFRAME.utils;
 var bind = utils.bind;
+
 
 
 AFRAME.registerComponent('selectable', {
@@ -26,28 +29,32 @@ AFRAME.registerComponent('selectable', {
         elem.addEventListener('click', function(evt) {
             var actualScene = elem.parentElement;
             var targetID = "#" + target;
-            console.log(targetID);
-
-
-
             var trg = sceneEl.querySelector(targetID);
-            console.log(trg);
 
-            var disappearAnimation = document.createElement('a-animation');
-            disappearAnimation.setAttribute('attribute', 'opacity');
-            disappearAnimation.setAttribute('dur', '4000');
-            disappearAnimation.setAttribute('from', '1');
-            disappearAnimation.setAttribute('to', '0');
 
-            var appearAnimation = document.createElement('a-animation');
+
+           actualScene.setAttribute('animation__disappear', 'property: material.opacity; dur: 4000; easing: linear; from: 1; to: 0;');
+           trg.setAttribute('animation__appear', 'property: material.opacity; dur:4000; easing:linear; from: 0; to: 1;')
+
+
+            //console.log(actualScene);
+            /*
+           var disappearAnimation = document.createElement('a-animation');
+           disappearAnimation.setAttribute('attribute', 'material.opacity');
+           disappearAnimation.setAttribute('dur', '4000');
+           disappearAnimation.setAttribute('from', '1');
+           disappearAnimation.setAttribute('to', '0');
+           disappearAnimation.setAttribute('easing', 'linear'); */
+
+            /*var appearAnimation = document.createElement('a-animation');
             appearAnimation.setAttribute('attribute', 'opacity');
             appearAnimation.setAttribute('dur', '4000');
             appearAnimation.setAttribute('from', '0');
             appearAnimation.setAttribute('to', '1');
+            appearAnimation.setAttribute('easing', 'linear');*/
 
-
-            actualScene.appendChild(disappearAnimation);
-            trg.appendChild(appearAnimation);
+            //actualScene.appendChild(disappearAnimation);
+            //trg.appendChild(appearAnimation);
             //sceneEl.querySelector(targetID).setAttribute('visible', true);
         })
     }
