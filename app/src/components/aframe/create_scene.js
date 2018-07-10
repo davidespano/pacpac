0f5c9d486e6ceb479e91bc5caa01ecdf331e57fd
Vaknar <Vaknarosh@gmail.com>
2018-07-10 10:38:02 +0200

    Trying to create play scene with React

--- app/src/components/aframe/create_scene.js
+++ app/src/components/aframe/create_scene.js
@@ -1,6 +1,8 @@
 import Transition from "../../interactives/Transition";
 import Scene from "../../scene/Scene";
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {Entity} from 'aframe-react';
 
 import InteractiveObject from "../../interactives/InteractiveObject";
 
@@ -64,7 +66,11 @@ function removeEventListeners (el, eventName, handlers) {
 
 AFRAME.registerComponent('create_scene', {
 
-    init : function () {
+
+    init: function (){
+        sceneCreator();
+    }
+    /*init : function () {
         sceneFactoryProv();
         //Cicleremo su tutti gli oggetti della scena(?)
         var sceneEl = document.querySelector('a-scene');
@@ -77,17 +83,17 @@ AFRAME.registerComponent('create_scene', {
         map.set('bolla2', tr3);
         map.set('bolla4', tr4);
 
-        bolla1.setAttribute('src', 'http://localhost:3000/media/prova2.mp4');
+        bolla1.setAttribute('src', 'http://localhost:3000/media/bolla1.jpg');
         bolla2.setAttribute('src', 'http://localhost:3000/media/bolla2.jpg');
         bolla3.setAttribute('src', 'http://localhost:3000/media/bolla3.jpg');
-        bolla4.setAttribute('src', 'http://localhost:3000/media/prova.mp4');
+        bolla4.setAttribute('src', 'http://localhost:3000/media/bolla4.jpg');
 
         /*bolla1.setAttribute('src', 'http://192.168.0.221:3000/media/prova.mp4');
         bolla2.setAttribute('src', 'http://192.168.0.221:3000/media/prova2.mp4');
         bolla3.setAttribute('src', 'http://192.168.0.221:3000/media/bolla3.jpg');
         bolla4.setAttribute('src', 'http://192.168.0.221:3000/media/bolla4.jpg');*/
 
-        bolla2.setAttribute('visible', 'false');
+       /* bolla2.setAttribute('visible', 'false');
         bolla3.setAttribute('visible', 'false');
         bolla4.setAttribute('visible', 'false');
 
@@ -123,9 +129,29 @@ AFRAME.registerComponent('create_scene', {
         sceneEl.appendChild(bolla2);
         sceneEl.appendChild(bolla3);
         sceneEl.appendChild(bolla4);
+        */
 
-    }
+    //}
 });
+
+function curvedImageFact(bolla, transitions, sel) {
+
+    transitions.forEach(function(x){
+        var event = document.createElement('a-curvedimage');
+        event.setAttribute('id', 'curv' + x.rules.target);
+        event.setAttribute('rotation', x.rotation);
+        event.setAttribute('radius', '9.5');
+        event.setAttribute('theta-length', x.theta);
+        event.setAttribute('height', x.height);
+        if(sel)
+        {
+            event.setAttribute('selectable', 'target: ' + x.rules.target)
+            console.log("Pippo:" + x.rules.target);
+        }
+
+        bolla.appendChild(event);
+    });
+}
 
 function curvedImage(bolla, target, rot, rad, theta, sel) {
     var event = document.createElement('a-curvedimage');
@@ -139,6 +165,9 @@ function curvedImage(bolla, target, rot, rad, theta, sel) {
 
     bolla.appendChild(event);
 }
+
+
+
 function foo(props){
     console.log("Dio " +props.ref)
 }
@@ -221,10 +250,70 @@ function sceneFactoryProv()
 {
     var sceneList = [];
 
-    var scene1 = new Scene([],'---','black',"bolla1.jpg");
-    scene1.img = "bolla1.jpg";
-    console.log("Mi piace la fava:" + scene1.name);
+    var scene1 = new Scene("bolla1.jpg");
+    var tr1 = new Transition('', 2000, '0 -90 0');
+    tr1.rules.target='bolla2';
+    //var tr2 = new Transition('', 2000, '0 0 0');
+    //tr2.rules.target='bolla2';
+    var tr4 = new Transition('', 2000, '0 180 0');
+
+    scene1.transitions.push(tr1);
+    //scene1.transitions.push(tr2);
+    sceneList.push(scene1);
+
+    var scene2 = new Scene("bolla2.jpg");
+    var tr3 = new Transition('', 2000, '0 90 0');
+    tr3.rules.target='bolla1';
+    scene2.transitions.push(tr3);
+    sceneList.push(scene2);
+
+    return sceneList;
 }
+
+function sceneCreator()
+{
+    var factory = sceneFactoryProv();
+    var element;
+
+    var sceneEl = document.querySelector('a-scene');
+    var first = true;
+    var array;
+    var i=0;
+    //Fallo diventare un bel foreach per cortesia
+    //for(element in factory)
+   // for(var i=0; i<factory.length; i++)
+    //{
+        //var bolla = document.createElement('a-sky');
+
+         array = (<Entity geometry = {{primitive: 'a-sky'}} id={{id: factory[i].name}}  material={{src: 'http://localhost:3000/media/' + factory[i].img}}
+            geometry ={{scale: '-1 1 1'}} radius = {{radius: 10}} />);
+         console.log(array);
+        /*bolla.setAttribute('primitive', 'a-sky');
+        bolla.setAttribute('id', factory[i].name);
+        bolla.setAttribute('src', 'http://localhost:3000/media/' + factory[i].img);
+        bolla.setAttribute('scale', '-1 1 1');
+        bolla.setAttribute('radius', '10');
+        if(!first)
+        {
+            bolla.setAttribute('visible', false);
+            bolla.setAttribute('opacity', '0');
+        }
+        else
+        {
+            curvedImageFact(bolla, factory[i].transitions, true);
+            first = false;
+        }*/
+
+        ReactDOM.render(array, document.getElementById("mainscene"));
+        //sceneEl.appendChild(bolla);
+        console.log(sceneEl);
+    //}
+
+}
+
+
+
+
 
 
 
