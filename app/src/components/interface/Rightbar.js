2a0c802d40c4f4a4839374af05006861024b5101
moku95 <marco.romoli@outlook.it>
2018-07-11 11:44:51 +0200

    migliorata visualizzazione rightbar, input solo numerici

--- app/src/components/interface/Rightbar.js
+++ app/src/components/interface/Rightbar.js
@@ -25,27 +25,81 @@ function optionsView(object,type){
     //lista visualizzazione per tipo ti oggetto
 
     if(type === InteractiveObjectsTypes.TRANSITION){
-        console.log("dfiohs")
         return(
         <div className={'currentObjectOptions'}>
             <label>media che fa cose</label>
-            <input value={"non ho ancora idea"}/>
-            <label>Duration</label>
-            <input value={object.duration}/>
-            <label>Rotation</label>
-            <input className={"rotationInput"} value={object.rotationX}/>
-            <input className={"rotationInput"} value={object.rotationY}/>
-            <input className={"rotationInput"} value={object.rotationZ}/>
-            <label>Theta</label>
-            <input value={object.theta}/>
-            <label>Height</label>
-            <input value={object.height}/>
+            <div>
+                <label>Nome</label>
+                <input
+                    id={"transitionName"}
+                    className={"transitionInput"}
+                    type={"text"}
+                    defaultValue={object.name}
+                />
+                <label>Duration</label>
+                <input id={"transitionDuration"}
+                       className={"transitionInput"}
+                       defaultValue={object.duration}
+                       type={"text"}
+                       onInput={()=> { onlyNumbers("transitionDuration")} }
+                />
+            </div>
+            <div>
+                <label>Rotation</label>
+                <input
+                       id={"rotationX"}
+                       className={"rotationInput"}
+                       defaultValue={object.rotationX}
+                       type={"text"}
+                       onInput={()=> { onlyNumbers("rotationX")} }
+                />
+                <input
+                    id={"rotationY"}
+                    className={"rotationInput"}
+                    defaultValue={object.rotationY}
+                    type={"text"}
+                    onInput={()=> { onlyNumbers("rotationY")} }
+                />
+                <input
+                    id={"rotationZ"}
+                    className={"rotationInput"}
+                    defaultValue={object.rotationZ}
+                    type={"text"}
+                    onInput={()=> { onlyNumbers("rotationZ")} }
+                />
+            </div>
+            <div>
+                <label>Theta</label>
+                <input
+                    id={"transitionTheta"}
+                    className={"transitionInput"}
+                    defaultValue={object.theta}
+                    type={"text"}
+                    onInput={()=> { onlyNumbers("transitionTheta")} }
+                />
+            </div>
+            <div>
+                <label>Height</label>
+                <input
+                    id={"transitionHeight"}
+                    className={"transitionInput"}
+                    defaultValue={object.height}
+                    type={"text"}
+                    onInput={()=> { onlyNumbers("transitionHeight")} }
+                />
+            </div>
         </div>
         )
     }
 
 
 }
-
+//https://stackoverflow.com/questions/8808590/html5-number-input-type-that-takes-only-integers/17208628
+function onlyNumbers(id) {
+    let prova =document.getElementById(id);
+    prova.value = prova.value.replace(/[^0-9.]/g, '');
+    prova.value = prova.value.replace(/(\..*)\./g, '$1');
+}
 
 export default RightBar;
+
