cda507a44a8fea9f8d734201328f2ee51cd65096
Vaknar <Vaknarosh@gmail.com>
2018-07-12 13:03:44 +0200

    Refactor selectable

--- app/src/components/aframe/create_scene.js
+++ app/src/components/aframe/create_scene.js
@@ -1,61 +1,12 @@
 import Transition from "../../interactives/Transition";
 import MyScene from "../../scene/MyScene";
+import './aframe-selectable'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Entity, Scene} from 'aframe-react';
 import InteractiveObject from "../../interactives/InteractiveObject";
 var AFRAME = require('aframe');
 var factory = sceneFactory();
-
-
-
-AFRAME.registerComponent('selectable',
-{
-    schema: {
-        target:{type: 'string'}
-    },
-
-    init: function () {
-        var sceneEl = document.querySelector('a-scene');
-        var elem = this.el;
-        var target = this.data.target;
-        var actualScene = elem.parentElement;
-        var targetID = "#" + target;
-        var trg = sceneEl.querySelector(targetID);
-        var cursor = sceneEl.querySelector('#cursor');
-
-        actualScene.setAttribute('animation__disappear', 'property: material.opacity; dur: 2000; easing: linear; from: 1; to: 0; startEvents: ' + actualScene.id + "dis");
-        trg.setAttribute('animation__appear', 'property: material.opacity; dur: 2000; easing: linear; from: 0; to: 1; startEvents: ' + trg.id + "app");
-
-        elem.addEventListener('mouseenter', function () {
-            cursor.setAttribute('color', 'green');
-        });
-        elem.addEventListener('mouseleave', function () {
-            cursor.setAttribute('color', 'black');
-        });
-
-        elem.addEventListener('click', function (evt) {
-            var disappear = new CustomEvent(actualScene.id + "dis");
-            var appear = new CustomEvent(trg.id + "app");
-            actualScene.dispatchEvent(disappear);
-            trg.dispatchEvent(appear);
-
-            trg.addEventListener("animationcomplete", function _listener(evt)
-            {
-                console.log("Animation");
-                if(evt.detail.name == "animation__appear") enableChild(trg);
-
-                trg.removeEventListener("animationcomplete", _listener);
-                this.components[evt.detail.name].animation.reset();
-            });
-
-            disableChilds(actualScene);
-        });
-    }
-
-
-});
-
 
 function curvedImageFact(transitions)
 {
@@ -147,7 +98,7 @@ export function sceneCreator()
    ReactDOM.render(tables, document.getElementById("mainscene"));
 }
 
-function enableChild(trg)
+export function enableChild(trg)
 {
     var element = factory.find(el => el.name == trg.id)
 
@@ -157,7 +108,7 @@ function enableChild(trg)
     }
 }
 
-function disableChilds(actualScene)
+export function disableChilds(actualScene)
 {
     ReactDOM.render([], actualScene);
 }
