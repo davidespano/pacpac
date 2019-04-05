1f650ad4c3cf828bda4c71791eac69da0492a2f0
server <server@spano-didattica.unica.it>
2019-04-05 19:27:18 +0200

    Configuration files to run on the public server

--- app/src/utils/settings.js
+++ app/src/utils/settings.js
@@ -1,11 +1,11 @@
-/**/module.exports = {
+/*module.exports = {
     apiBaseURL: 'http://localhost:3000/api/v0',
     mediaURL: 'http://localhost:3000/media/',
 };
- /*
+ */
 
 module.exports = {
-    apiBaseURL: 'http://90.147.146.244/api/v0',
-    mediaURL: 'http://90.147.146.244/media/',
+    apiBaseURL: 'http://cg3hci.dmi.unica.it/api/v0',
+    mediaURL: 'http://cg3hci.dmi.unica.it/media/',
 };
 /**/
