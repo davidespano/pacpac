3b9a3ae4fd9d32c9d999e8e9ddc911fbc6763d4a
fabiomurru96 <fabiomurru96@hotmail.it>
2019-06-07 18:17:32 +0200

    Progress in thumbnails, to be finished

--- api/handleFilemanagerAPI.js
+++ api/handleFilemanagerAPI.js
@@ -1,11 +1,12 @@
 const path = require('path')
     , loginRequired = require('./middlewares/loginRequired').loginRequired
-    ,sharp = require("sharp")
+    , sharp = require("sharp")
     , filemanagerMiddleware = require('@opuscapita/filemanager-server').middleware
     , ffmpeg = require('fluent-ffmpeg')
     , ffmpegPath = require('ffmpeg-static').path
     , ffprobePath = require('ffprobe-static').path
-    , fs = require('fs').promises;
+    , fs = require('fs').promises
+    , dbUtils = require('./neo4j/dbUtils');
 
 const fileManagerConfig = {
     fsRoot: path.resolve(__dirname, 'public'),
@@ -13,75 +14,123 @@ const fileManagerConfig = {
 };
 
 ffmpeg.setFfmpegPath(ffmpegPath);
-ffmpeg.setFfprobePath(ffprobePath);
 
-function genVideoThumbnail(file, dir){
+//function save
+
+function genVideoScreenshot(file, dir){
+    const output = dir+'/_thumbnails_/'+file.filename+'.png';
     return new Promise((resolve,reject) =>{
         ffmpeg(dir+'/'+file.filename)
-            .output(dir+'/_thumbnails_/'+file.filename+'.png')
-            .noAudio()
-            .size('300x?')
             .on('error', (err)=>{
                 reject(err);
             })
-            .on('end', () => resolve())
-            .run();
+            .on('end', () => {
+                console.log('genVideoScreenshot', output);
+                resolve(output)
+            })
+            .screenshots({
+                count: 1,
+                folder: dir+'/_thumbnails_',
+                filename: file.filename + '.png'
+            });
     })
 }
 
-function createThumbnails(files, dir){
+async function createThumbnails(files, dir){
     const promises = [];
 
-    promises.push(fs.mkdir(dir + '/_thumbnails_').catch((err) => console.error(err)));
+    await fs.mkdir(dir + '/_thumbnails_').catch(() => {});
 
     files.forEach((file) => {
-        console.log(file);
+        let promise;
+
         if (file.mimetype.includes("video"))
-            promises.push(genVideoThumbnail(file, dir).catch(err => console.error(err)));
+            promise = (genVideoScreenshot(file, dir).catch(err => console.error(err)));
         else
-            promises.push(sharp(dir + '/' + file.filename).resize(300)
-                .toFile(dir + '/_thumbnails_/' + file.filename)
-                .catch((err) => console.error(err)));
+            promise = new Promise((resolve,reject) => resolve(dir + '/' + file.filename));
+
+
+        //take metadata with sharp and resize image
+        promise = promise.then((res) => {
+            console.log(res);
+            const image = sharp(res);
+            return image.metadata().then((metadata) => {
+                console.log(metadata.width+"x"+metadata.height);
+
+                //using a buffer to override the old image
+                return image
+                    .resize(300)
+                    .toBuffer(function(err, buffer) {
+                        fs.writeFile(dir + '/_thumbnails_/' + path.parse(res).base, buffer, function(e) {
+
+                        });
+                    });
+            })}).catch(err => console.error(err));
+
+        promises.push(promise);
     });
-    return Promise.all(promises);
+    await Promise.all(promises);
 }
 
 
 function routeHandler(req, res, next){
-        const dir = path.resolve(__dirname, 'public' + '/' + req.params.gameID);
+    const dir = path.resolve(__dirname, 'public' + '/' + req.params.gameID);
 
-        //File upload
-        if (req.method === "POST" && req.path === "/files") {
-            const send = res.send;
-            console.log(req.files);
-            //override send function, used to check when the file is uploaded completely
-            res.send = function (body) {
-                //Creation of the thumbnails and call of the former send fuction
-                createThumbnails(req.files, dir)
-                    .then(send.call(this, body));
-            }
+    //File upload
+    if (req.method === "POST" && req.path === "/files") {
+        const send = res.send;
+        console.log(req.files);
+        //override send function, used to check when the file is uploaded completely
+        res.send = function (body) {
+            //Creation of the thumbnails and call of the former send fuction
+            createThumbnails(req.files, dir)
+                .then(send.call(this, body));
         }
-        //File remove
-        else if (req.method === "DELETE") {
+    }
+    //File remove
+    else if (req.method === "DELETE") {
 
-            //Delete the thumbnails
-            let regex = RegExp('.*\.mp4$');
-            let filename = regex.test(req.body.name) ? (req.body.name + '.png') : req.body.name;
-            fs.unlink(dir + '/_thumbnails_/' + filename)
-                .then(() => console.log('file deleted'))
-                .catch((err) => console.error("delete",err));
-        }
+        //Delete the thumbnails, if it's an mp4 appends .png at the end
+        let regex = RegExp('.*\.mp4$');
+        let filename = regex.test(req.body.name) ? (req.body.name + '.png') : req.body.name;
+        fs.unlink(dir + '/_thumbnails_/' + filename)
+            .then(() => console.log('file deleted'))
+            .catch((err) => console.error("delete",err));
+    }
 }
 
+function uploadHandler(req, res, next) {
+    const dir = path.resolve(__dirname, 'public' + '/' + req.params.gameID);
+    const send = res.send;
+    console.log(req.files);
+//override send function, used to check when the file is uploaded completely
+    res.send = function (body) {
+        //Creation of the thumbnails and call of the former send fuction
+        createThumbnails(req.files, dir)
+            .then(() => send.call(this, body));
+    };
+    next();
+}
+
+function deleteHandler(req, res, next){
+    const dir = path.resolve(__dirname, 'public' + '/' + req.params.gameID);
+    let regex = RegExp('.*\.mp4$');
+    let filename = regex.test(req.body.name) ? (req.body.name + '.png') : req.body.name;
+    fs.unlink(dir + '/_thumbnails_/' + filename)
+        .then(() => console.log('file deleted'))
+        .catch((err) => console.error("delete",err));
+    next()
+}
 
 function handleFilemanagerAPI(api)
 {
+    api.post('/filemanager/:gameID/files', uploadHandler);
+    api.delete('/filemanager/:gameID/*', deleteHandler);
     api.use('/filemanager/:gameID', loginRequired, (req, res, next) => {
         fileManagerConfig.fsRoot = path.resolve(__dirname, 'public') + '/' + req.params.gameID;
         const router = filemanagerMiddleware(fileManagerConfig);
         router(req, res, next);
-        next();
-    }, routeHandler);
+    });
 }
 
 module.exports = {
