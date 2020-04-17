28f8e651b07cdb205236674920fbaa72a41b5057
cristinapinna <p.cristaina@gmail.com>
2020-04-17 17:05:16 +0200

    Inizio salvataggi

--- app/src/data/EditorStateStore.js
+++ app/src/data/EditorStateStore.js
@@ -5,6 +5,7 @@ import EditorState from "./EditorState";
 import Immutable from "immutable";
 import SceneAPI from "../utils/SceneAPI";
 import ScenesStore from "./ScenesStore";
+import DebugAPI from "../utils/DebugAPI";
 
 class EditorStateStore extends ReduceStore {
 
