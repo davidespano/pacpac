6ee2d935e6c3551ac46c5c0b5b1d84d78b687130
TurnuAlessandro <48202020+TurnuAlessandro@users.noreply.github.com>
2020-04-21 19:10:02 +0200

    InputSaveForm does not refer to EditorState
    InputSaveForm does not refer to EditorState

--- app/src/components/interface/InputSaveForm.js
+++ app/src/components/interface/InputSaveForm.js
@@ -95,6 +95,26 @@ function InputSaveForm(props) {
         );
     }
 
+    console.clear();
+    console.log("props.debugRunState", props.debugRunState);
+
+    if(props.debugRunState === undefined){
+        return (
+            <div id={"register"}>
+                <div className="modal fade" id="save-modal" tabIndex="-1" role="dialog"
+                     aria-labelledby="register-modal-label" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="register-modal-label">Non ci sono modifiche</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div id={"register"}>
             <div className="modal fade" id="save-modal" tabIndex="-1" role="dialog"
@@ -133,6 +153,7 @@ function InputSaveForm(props) {
                             <button type="button"
                                     id="save-submit-button"
                                     className="btn btn-secondary buttonConferm"
+
                                     onClick={() => {saveForm(props); clearForm()}}
                                     data-dismiss="modal"
                                     >
